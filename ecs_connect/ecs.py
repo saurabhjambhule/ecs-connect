b78483e44f60da8d232173303da6edd59001c5e4
Saurabh Jambhule <sjambhule@g2.com>
2020-03-03 20:26:24 +0530

    add ecs handler

---
+++ ecs_connect/ecs.py
@@ -0,0 +1,45 @@
+"""Main module."""
+
+import os
+import boto3
+from botocore.exceptions import ClientError
+
+class ECSHandler():
+    """ ECS handler  """
+    def __init__(self, cluster, service, logger):
+        self.cluster = cluster
+        self.service = service
+        self.logger = logger
+        self.ecs_client = boto3.client('ecs')
+
+    def get_task_id(self):
+        response = self.ecs_client.list_tasks(
+            cluster=self.cluster,
+            serviceName=self.service,
+            desiredStatus='RUNNING'
+        )
+
+        return response['taskArns'][0]
+
+    def get_container_instance_id(self):
+        task_id = self.get_task_id()
+        response = self.ecs_client.describe_tasks(
+            cluster=self.cluster,
+            tasks=[
+                task_id,
+            ]
+        )
+
+        return response['tasks'][0]['containerInstanceArn']
+
+    def get_ec2_instance_id(self):
+        container_instance_id = self.get_container_instance_id()
+
+        response = self.ecs_client.describe_container_instances(
+            cluster=self.cluster,
+            containerInstances=[
+                container_instance_id,
+            ]
+        )
+
+        return response['containerInstances'][0]['ec2InstanceId']
